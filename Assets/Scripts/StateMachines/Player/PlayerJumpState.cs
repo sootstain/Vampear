439bd390a9dfbdb3535acb7a678c34e8f2525151
YCooE <31769542+YCooE@users.noreply.github.com>
2025-11-05 02:25:27 +0100

    walking and jumping feelsgoodman

--- Assets/Scripts/StateMachines/Player/PlayerJumpState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerJumpState.cs
@@ -5,11 +5,15 @@ public class PlayerJumpState : PlayerBaseState
     private readonly int JumpRef = Animator.StringToHash("Jump");
     private const float CrossFadeDuration = 0.1f;
     private Vector3 momentum;
+    
+    private const float SnapAngleThreshold = 120f;
+    private const float RotationSpeed = 20f;
 
     public PlayerJumpState(PlayerStateMachine stateMachine) : base(stateMachine) 
     {
         
     }
+    
 
     public override void Enter()
     {
@@ -18,6 +22,11 @@ public class PlayerJumpState : PlayerBaseState
         momentum.y = 0f; //only jump determines y movement
         stateMachine.Animator.CrossFadeInFixedTime(JumpRef, CrossFadeDuration);
         stateMachine.LedgeDetection.OnLedgeDetected += HandleLedgeDetection;
+        if (stateMachine.InputReader.MovementValue.sqrMagnitude > Mathf.Epsilon)
+        {
+            Vector3 jumpDirection = CalculateJump();
+            FaceJumpDirection(jumpDirection, Time.deltaTime);
+        }
     }
 
     public override void Exit()
@@ -41,5 +50,45 @@ public class PlayerJumpState : PlayerBaseState
     private void HandleLedgeDetection(Vector3 ledgeForward)
     {
         stateMachine.SwitchState(new PlayerHangState(stateMachine, ledgeForward));
+    }
+    
+    private Vector3 CalculateJump()
+    {
+        Vector3 forward = stateMachine.MainCameraPosition.forward;
+        Vector3 right = stateMachine.MainCameraPosition.right;
+
+        //don't care about vertical pos
+        forward.y = 0;
+        right.y = 0;
+        
+        forward.Normalize();
+        right.Normalize();
+
+        return forward * stateMachine.InputReader.MovementValue.y + 
+               right * stateMachine.InputReader.MovementValue.x;
+    }
+    
+    private void FaceJumpDirection(Vector3 movement, float deltaTime)
+    {
+        if (movement.sqrMagnitude > Mathf.Epsilon)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            float angleDifference = Quaternion.Angle(stateMachine.transform.rotation, targetRotation);
+            if (angleDifference > SnapAngleThreshold)
+            {
+                stateMachine.transform.rotation = targetRotation;
+            }
+            else
+            {
+                // Slerp is used
+                // BUT Quaternion.RotateTowards can be used for framerate independent fast rotation
+                float rotationAmount = Mathf.Min(1f, RotationSpeed * deltaTime);
+                stateMachine.transform.rotation = Quaternion.Slerp(
+                    stateMachine.transform.rotation, 
+                    targetRotation, 
+                    rotationAmount
+                );
+            }
+        }
     }
 }

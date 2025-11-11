ce2906b09b658594f12d7e18808c30acf88e1ee4
sootstain <Sophie.Bielleman@me.com>
2025-11-11 04:19:34 +0100

    Creating Prototype Scene

--- Assets/Scripts/Dialogue/Trigger.cs
+++ Assets/Scripts/Dialogue/Trigger.cs
@@ -1,11 +1,16 @@
+using System;
 using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
+    //for scene triggers
     public GameEvent gameEvent;
+    public bool isTriggered;
 
-    public void Invoke()
+    public void OnTriggerEnter(Collider other)
     {
-        
+        if (isTriggered) return;
+        gameEvent.TriggerEvent();
+        isTriggered = true;
     }
 }
